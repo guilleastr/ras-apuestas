bab66d457d57980cb0007efa26d2ad1058d2206e
UO269763 <e10263@alunos.uminho.pt>
2021-11-16 18:43:15 +0000

    registro hecho

--- modules/gestorDB.js
+++ modules/gestorDB.js
@@ -4,5 +4,41 @@ module.exports = {
     init: function (app, mongo) {
         this.mongo = mongo;
         this.app = app;
+    },
+    // Añadimos un nuevo usuario a la base
+    insertarUsuario : function(usuario, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('usuarios');
+                collection.insert(usuario, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    // Obtenemos los  usuarios que se encuentren en la base de datos
+    obtenerUsuarios : function(criterio,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('usuarios');
+                collection.find(criterio).toArray(function(err, usuarios) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(usuarios);
+                    }
+                    db.close();
+                });
+            }
+        });
     }
 };
